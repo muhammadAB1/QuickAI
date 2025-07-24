c49911f4dfef80d22f6a83ac4c5e24c8b6e0568b
muhammadAB1 <abrarjpj7@gmail.com>
2025-07-24 08:40:45 +0500

    Change AWS environment variable

--- Backend/configs/awsS3.js
+++ Backend/configs/awsS3.js
@@ -4,8 +4,8 @@ import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 const s3Client = new S3Client({
     region: 'ap-south-1',
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEYS,
+        secretAccessKey: process.env.AWS_SECRET_KEYS
     }
 })
 
